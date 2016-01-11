815599cc488f975fda9e52272ba8b76349d918b1
kshy <habxp@163.com>
2016-01-11 17:28:57 +0800

    头部固定、模板更新、渲染正常

--- app/js/scfe/material/index.js
+++ app/js/scfe/material/index.js
@@ -14,17 +14,17 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
         }
 
         var index = {
-            tpl:require("app/modules/tmpl/material/index"),
+            tpl: require("app/modules/tmpl/material/index"),
             init: function() {
 
                 this.initEvent();
 
             },
             initEvent: function() {
-                
+
+                this.loadContent(this.tpl);
 
                 this.initCategory(this.tpl);
-                this.loadContent(this.tpl);
             },
             initCategory: function(tpl) {
                 //初始化分类
@@ -43,7 +43,7 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
                             slidesPerView: 5,
 
                             speed: 400,
-                            //    spaceBetween: 100,
+                            //spaceBetween: 100,
 
                         });
                         //启动Swiper
@@ -66,13 +66,14 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
                             slideNextClass: 'bar_slider_next',
                             //上个slider
                             slidePrevClass: 'bar_slider_prev'
-                            //spaceBetween: 30
+                                //spaceBetween: 30
 
                         });
                         //添加点击事件
                         xswiper.on("onClick", function(sw) {
 
-                            var current =$(sw.clickedSlide),currentId=current.attr("data-category");
+                            var current = $(sw.clickedSlide),
+                                currentId = current.attr("data-category");
 
                             current.hasClass('selected') || current.addClass('selected').siblings().removeClass('selected');
                             //调用OC暴露的api
@@ -80,16 +81,19 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
                                 categoryId: currentId,
                                 success: true
                             }, function(response) {
-                                console.log("JS已经发出,同时收到回调，说明OC已经收到数据");
                                 //可以更简单的
-                                index.loadContent(tpl,currentId);
+                                index.loadContent(tpl);
                             });
-                // index.loadContent(cu)
                         });
+                        //初始化 
+                        //陷阱
+                        bridge.callHandler('categoryInitialized', {
+                            categoryId: $(xswiper.slides[0]).attr('data-category'),
+                            success: true
+                        }, function(response) {
+                            index.loadContent(tpl, $(xswiper.slides[0]).attr('data-category'));
 
-                              xswiper.slides[0].click;
-
-
+                        });
                         if (responseCallback) {
                             responseCallback({
                                 success: true
@@ -100,35 +104,61 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
 
                 })
             },
-            loadContent: function(tpl,materialId) {
-                debugger
+            loadContent: function(tpl) {
+
                 connectWebViewJavascriptBridge(function(bridge) {
                     //js暴露的api
                     bridge.registerHandler("loadContent", function(data, responseCallback) {
-                    $('.cardlist_wrapper').html('');
-                        $('.cardlist_wrapper').append(tpl.List(data))
+                        //todo:这里可以插入loading的方法
+                        $('.cardlist_wrapper').html('');
+                        //正则需要处理
+                        if (data && data.length >= 0) {
+                            data.forEach(function(oj) {
+                                var content = oj.textContent;
+                                if (content.trim() !== "" && content !== undefined) {
+                                    //#标签匹配 遍历
+                                    var sharpRegArr = content.match(/#[\u4e00-\u9fa5]*#/g);
+                                    sharpRegArr && sharpRegArr.length > 0 && sharpRegArr.forEach(function(obj) {
+                                        content = content.replace(obj, "<span class='tag'>" + obj + "</span>");
+                                    });
+                                    //match 遍历
+                                    var linkRegArr = content.match(/(http:\/\/\S+)/g);
+                                    linkRegArr && linkRegArr.length > 0 && linkRegArr.forEach(function(obj) {
+                                        content = content.replace(obj, "<a class='link' href='" + obj + "'>网页链接</a>");
+                                    });
+                                }
+                                oj.textContent = content;
+                            })
+                        }
 
+                        $('.cardlist_wrapper').append(tpl.List(data));
 
                         $(document).on("click", ".card_handlebar a", function() {
-                            // debugger
                             //bridge侵占this
-                            var self=this;
+                            var self = this,
+                                materialId = $(self).parent(".card_handlebar").attr("data-materialId");
                             if ($(self).index() === 0) {
-                                bridge.callHandler('useBtnClicked', {}, function(message) {
+                                bridge.callHandler('useBtnClicked', {
+                                    materialId: materialId
+                                }, function(message) {
                                     console.log('如果看到这个只能说明跳转失败');
                                 });
 
                             } else {
-                                var flag=!$(self).hasClass("chosen")
-                                //调用OC暴露的api
+                              
+                                var flag = !$(self).hasClass("chosen")
+                                    //调用OC暴露的api
                                 bridge.callHandler('favoriteBtnClicked', {
-                                        isNowFavorite:flag,
-                                        materialId:123
+                                        isNowFavorite: flag,
+                                        materialId: materialId
                                     },
                                     function(message) {
+                                        if (message) {
+                                            flag && $(self).addClass("chosen") || $(self).removeClass("chosen");
+
+                                        }
                                         console.log('这里是点击时间传到OC端确认后回调');
                                         //这里this指向了window 因为bridge闭包的原因
-                                        flag&&$(self).addClass("chosen")||$(self).removeClass("chosen");
                                     });
                             }
                         });
@@ -146,9 +176,4 @@ define('app/modules/scfe/material/index', function(require, exports, module) {
         };
         module.exports = index;
     })(jquery);
-
-
-
-
-
 });
